56c2b3e5b9ab1d5298b032b89c326854ef5e9d15
Anton Kreuzkamp <anton.kreuzkamp@kdab.com>
2016-03-22 13:13:36 +0100

    Make saving the environment work.
    set > /some/file would work nicely if all environment variables were
    well-behaved. Unfortunately the environment has one variable called
    "affinity:container" and exporting that would fail because of the
    colon. Thus we now only export the variables we want. It requires us
    to adjust this script every time we add some environment variable, but
    at least it works.

--- scripts/run_cron.sh
+++ scripts/run_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-set | sed 's/^/export /' > /var/tmp/env.sh
+echo "export DOMAINS=\"$DOMAINS\"" > /var/tmp/env.sh
+echo "export EMAIL=\"$EMAIL\"" >> /var/tmp/env.sh
+echo "export DH_PARAMETERS=\"$DH_PARAMETERS\"" >> /var/tmp/env.sh
+echo "export MERGE_KEY_WITH_CERTIFICATE=\"$MERGE_KEY_WITH_CERTIFICATE\"" >> /var/tmp/env.sh
 
 cron -f
